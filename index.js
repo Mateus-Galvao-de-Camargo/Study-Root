2e49a8d5f1bf897c8d9794feee3f8832c64c4cb8
gabriel282828 <ga04789135373@gmail.com>
2023-03-21 20:09:16 -0300

    atualiza a pasta certa garai
    alo garai

---
+++ index.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow} = require('electron')
+
+let janelaPrincipal
+
+app.on('ready', () => {
+    janelaPrincipal = new BrowserWindow({
+        height: 900,
+        width: 1200,
+        resizable: false
+    })
+
+    janelaPrincipal.loadURL(`file://${__dirname}/index.html`)
+})
