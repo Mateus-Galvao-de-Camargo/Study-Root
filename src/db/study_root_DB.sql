7b0181ec8077605893ed15bb5084b324a2786a17
Gabriel Lopes <ga04789135373@gmail.com>
2023-03-23 16:42:39 -0300

    atualizas

--- src/db/study_root_DB.sql
+++ src/db/study_root_DB.sql
@@ -2,6 +2,14 @@ CREATE DATABASE IF NOT EXISTS study_root;
 
 USE study_root;
 
+CREATE TABLE estudante(
+  id_estudante INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR NOT NULL,
+  senha VARCHAR NOT NULL,
+);
+
+
+
 CREATE TABLE assunto(
   id_assunto INT NOT NULL AUTO_INCREMENT,
   titulo VARCHAR NOT NULL,
