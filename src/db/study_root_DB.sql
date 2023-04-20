107cf3d3f21aaf267a3714c58f03f98a2c550e62
Mateus Galvão de Camargo <mateus.galvao.camargo.contato@gmail.com>
2023-04-19 21:11:22 -0300

    botão de fechar, denovo
    Esse botão só da BO, mas deve tar resolvido de uma vez por todas🦍

--- src/db/study_root_DB.sql
+++ src/db/study_root_DB.sql
@@ -3,14 +3,14 @@ CREATE DATABASE IF NOT EXISTS study_root;
 USE study_root;
 
 CREATE TABLE IF NOT EXISTS estudante(
-  id_estudante INT NOT NULL AUTO_INCREMENT,
+  id_estudante INT(11) NOT NULL AUTO_INCREMENT,
   usuario VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   senha VARCHAR(37) NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS assunto(
-  id_assunto INT NOT NULL AUTO_INCREMENT,
+  id_assunto INT(11) NOT NULL AUTO_INCREMENT,
   titulo VARCHAR(52) NOT NULL,
   resumo VARCHAR(300),
   id_estudante_fk VARCHAR NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS assunto(
 );
 
 CREATE TABLE IF NOT EXISTS anotacao(
-  id_anotacao INT NOT NULL AUTO_INCREMENT,
+  id_anotacao INT(11) NOT NULL AUTO_INCREMENT,
   conteudo TEXT COLLATE utf8_unicode_ci NOT NULL,
   id_assunto_fk VARCHAR NOT NULL,
   PRIMARY KEY (id_anotacao),
