48f45a689fd0180d1136faaa25f8f26acf17981f
Mateus Galvão de Camargo <mateus.galvao.camargo.contato@gmail.com>
2023-05-14 22:05:43 -0300

    gelo?

--- src/back-end/cadastro_assunto.php
+++ src/back-end/cadastro_assunto.php
@@ -12,11 +12,15 @@
 
         $tamanhoDoTitulo = mb_strlen($tituloFormatado);
 
-        if($titulo == NULL || $titulo = "" || $tituloFormatado == NULL || $tamanhoDoTitulo > 52){
+        $resumoFormatado = trim(preg_replace('/\s+/', ' ', $resumo));
+
+        $tamanhoDoResumo = mb_strlen($resumoFormatado);
+
+        if($titulo == NULL || $titulo = "" || $tituloFormatado == NULL || $tamanhoDoTitulo > 52 || $tamanhoDoResumo > 300){
             print "<script>alert('Sem gracinhas, tente denovo, da maneira correta, o título é obrigatório, deve conter no máximo 52 caractéres e não pode ser vazio ou apenas conter espaços em branco.'); location.href='../telas/home.php'</script>";
         }
 
-        $row = $conn->query("INSERT INTO assunto (id_assunto, titulo, resumo, id_estudante_fk) VALUES (NULL, '$tituloFormatado', '$resumo', '$estudante');");
+        $row = $conn->query("INSERT INTO assunto (id_assunto, titulo, resumo, id_estudante_fk) VALUES (NULL, '$tituloFormatado', '$resumoFormatado', '$estudante');");
 
         if($row){
             print "<script>location.href='../telas/home.php'</script>";
--- src/back-end/update_assunto.php
+++ src/back-end/update_assunto.php
@@ -12,11 +12,15 @@
 
         $tamanhoDoTitulo = mb_strlen($tituloFormatado);
 
-        if($titulo == NULL || $titulo = "" || $tituloFormatado == NULL || $tamanhoDoTitulo > 52){
+        $resumoFormatado = trim(preg_replace('/\s+/', ' ', $resumo));
+
+        $tamanhoDoResumo = mb_strlen($resumoFormatado);
+
+        if($titulo == NULL || $titulo = "" || $tituloFormatado == NULL || $tamanhoDoTitulo > 52 || $tamanhoDoResumo > 300){
             print "<script>alert('Sem gracinhas, tente denovo, da maneira correta, o título é obrigatório, deve conter no máximo 52 caractéres e não pode ser vazio ou apenas conter espaços em branco.'); location.href='../telas/home.php'</script>";
         }
 
-        $res = $conn->query("UPDATE assunto SET titulo = '$titulo', resumo = '$resumo' WHERE id_assunto = $idAssunto AND id_estudante_fk = $estudante");
+        $res = $conn->query("UPDATE assunto SET titulo = '$tituloFormatado', resumo = '$resumoFormatado' WHERE id_assunto = $idAssunto AND id_estudante_fk = $estudante");
 
         if($res){
             print "<script>location.href='../telas/home.php'</script>";
