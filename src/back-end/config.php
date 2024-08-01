a3561356bd39fd766ed14fcedfc6d44234bb6451
Mateus-Galvao-de-Camargo <mateus.galvao.camargo.contato@gmail.com>
2024-08-01 00:37:13 -0300

    ADD Variaveis de ambiente

--- src/back-end/config.php
+++ src/back-end/config.php
@@ -1,7 +1,8 @@
 <?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'study_root');
 
-    $conn = new Mysqli(HOST, USER, PASS, BASE);
+$path = dirname(__FILE__, 2);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$conn = new Mysqli($_ENV['HOST'], $_ENV['USER'], $_ENV['PASS'], $_ENV['BASE']);
